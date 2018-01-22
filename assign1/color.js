4d36cd5eff3e80c45e7d243524fe3da6b646a510
James Hooper <jameshooperdesigns@yahoo.com>
2018-01-22 08:43:09 -0700

    First version of assignment 1 and 2

---
+++ assign1/color.js
@@ -0,0 +1,7 @@
+/* JavaScript */
+
+var firstName, lastName, favColor;
+firstName = prompt('What is your first name?');
+lastName = prompt('What is your last name?');
+favColor = prompt('What is your favorite color?');
+alert('Hello ' + firstName + " " + lastName + ". " + favColor + " is my favorite color.");
